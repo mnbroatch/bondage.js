46224f98ca2ba95130e098504897847b55c6109f
mark <alforno@gmail.com>
2021-12-28 12:41:54 -0800

    Fix whitespace after inline expr.
    This solution was originally implemented in mnbroatch's fork of my fork. It works well.

--- src/lexer/lexer.js
+++ src/lexer/lexer.js
@@ -162,16 +162,23 @@ class Lexer {
       }
 			
 			// Remove leading whitespace characters.
-      const spaceMatch = this.getCurrentLine().substring(this.yylloc.last_column - 1).match(/^\s*/);
-
-			if (spaceMatch.length !== 0) {
+      //const spaceMatch = this.getCurrentLine().substring(this.yylloc.last_column - 1).match(/^\s*/);
+			/*if (spaceMatch.length !== 0) {
         if (spaceMatch[0].length == 2) {
           this.yylloc.last_column += 1;
         } else {
           this.yylloc.last_column += spaceMatch[0].length;
         }
-      }
+      }*/
 
+			if (rule.token !== 'EndInlineExp') {
+				// Remove leading whitespace characters
+				const spaceMatch = this.getCurrentLine().substring(this.yylloc.last_column - 1).match(/^\s*/);
+				if (spaceMatch.length !== 0) {
+					this.yylloc.last_column += spaceMatch[0].length;
+				}
+			}
+				
       return rule.token;
     }
 
--- tests/test_parser.js
+++ tests/test_parser.js
@@ -392,7 +392,7 @@ describe('Parser', () => {
 				new nodes.VariableNode('testvar'),
 				new nodes.NumericLiteralNode('1'))
 				, { first_line: results[0].lineNum }),
-			new nodes.TextNode('test.', { first_line: results[0].lineNum })
+			new nodes.TextNode(' test.', { first_line: results[0].lineNum })
     ];
 
     expect(results).to.deep.equal(expected);
@@ -597,7 +597,7 @@ describe('Parser', () => {
 				new nodes.NumericLiteralNode('2'),
 				new nodes.NumericLiteralNode('2'))
 				, { first_line: results[0].lineNum }),
-			new nodes.TextNode('test.', { first_line: results[0].lineNum })
+			new nodes.TextNode(' test.', { first_line: results[0].lineNum })
     ];
 
     expect(results).to.deep.equal(expected);

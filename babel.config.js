c1e5609cbde636220e9510edb1b9bd520ed62de8
Matthew Broatch <mnbroatch@gmail.com>
2022-01-06 00:46:16 -0800

    wip build tools

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "plugins": [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-logical-assignment-operators",
+    "@babel/plugin-proposal-optional-chaining",
+  ]
+}
---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    format: 'umd',
+    name: 'bondage.js',
+  },
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    terser(),
+  ],
+};
+export default config;

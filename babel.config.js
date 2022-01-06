ce7c6fa0be3e5a5703a48fc9a2c2e9caf41e2943
Matthew Broatch <mnbroatch@gmail.com>
2022-01-06 01:18:41 -0800

    fix build

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  "plugins": [
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-proposal-logical-assignment-operators",
-    "@babel/plugin-proposal-optional-chaining",
-  ]
-}
+  // presets: [
+  //   ["@babel/preset-env", {"modules": false} ]
+  // ],
+};
+

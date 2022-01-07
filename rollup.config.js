498189346264091fc6f4dcc3d381b5544996d0b8
Matthew Broatch <mnbroatch@gmail.com>
2022-01-06 22:01:54 -0800

    further messing with build

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,32 @@
 import babel from '@rollup/plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
-const config = {
-  input: 'index.js',
-  output: {
-    format: 'umd',
-    name: 'bondage.min.js',
+const config = [
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/bondage.js',
+      format: 'umd',
+      name: 'bondage.js',
+    },
+    plugins: [
+      babel({ exclude: [] }),
+      commonjs(),
+    ],
   },
-  plugins: [
-    babel({ exclude: 'node_modules/**' }),
-    terser(),
-  ],
-};
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/bondage.min.js',
+      format: 'umd',
+      name: 'bondage.js',
+    },
+    plugins: [
+      babel({ exclude: [] }),
+      commonjs(),
+      terser(),
+    ],
+  },
+];
 export default config;
